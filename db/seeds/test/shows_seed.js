878da6e00064a3eed8ac506324179d1f331393ac
Cedric Chee <cedric@invictusbyte.com>
2020-07-01 14:50:05 +0800

    Knex seeds
    Populate the development and test database with initial data

---
+++ db/seeds/test/shows_seed.js
@@ -0,0 +1,38 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('shows')
+      .del()
+      .then(function () {
+          // Inserts seed entries
+          return knex('shows').insert([
+              {
+                  name: 'Suits',
+                  channel: 'USA Network',
+                  genre: 'Drama',
+                  rating: 3,
+                  explicit: false,
+              },
+              {
+                  name: 'Game of Thrones',
+                  channel: 'HBO',
+                  genre: 'Fantasy',
+                  rating: 5,
+                  explicit: true,
+              },
+              {
+                  name: 'South Park',
+                  channel: 'Comedy Central',
+                  genre: 'Comedy',
+                  rating: 4,
+                  explicit: true,
+              },
+              {
+                  name: 'Mad Men',
+                  channel: 'AMC',
+                  genre: 'Drama',
+                  rating: 3,
+                  explicit: false,
+              },
+          ]);
+      });
+};
