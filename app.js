da1e5e70af85affe53ad75cdba6ee4cdc5564b4a
Cedric Chee <cedric@invictusbyte.com>
2020-07-01 15:08:28 +0800

    Set up tests
    Install:
    * Mocha (test runner)
    * Chai (assertions)
    * ChaiHTTP (HTTP request module for integration testing)
    
    Modify:
    * Prevents application log messages from displaying in the stdout when
    the tests are ran
    * Make sure the error handlers return JSON

--- app.js
+++ app.js
@@ -8,13 +8,46 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV !== 'test') {
+    app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/v1', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err,
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {},
+    });
+});
 
 module.exports = app;
