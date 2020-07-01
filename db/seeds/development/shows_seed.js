eb4397f71953e7f99d4e26f75c41df3979625c6e
Cedric Chee <cedric@invictusbyte.com>
2020-07-01 19:06:25 +0800

    Reduce verbosity, similar to test environment seed

--- db/seeds/development/shows_seed.js
+++ db/seeds/development/shows_seed.js
@@ -1,38 +1,45 @@
-exports.seed = function (knex) {
-    // Deletes ALL existing entries
-    return knex('shows')
-        .del()
-        .then(function () {
-            // Inserts seed entries
-            return knex('shows').insert([
-                {
+exports.seed = (knex) => {
+    return (
+        knex('shows')
+            // Deletes ALL existing entries
+            .del()
+
+            // Then, inserts seed entries one by one in series
+            .then(() =>
+                knex('shows').insert({
                     name: 'Suits',
                     channel: 'USA Network',
                     genre: 'Drama',
                     rating: 3,
                     explicit: false,
-                },
-                {
+                })
+            )
+            .then(() =>
+                knex('shows').insert({
                     name: 'Game of Thrones',
                     channel: 'HBO',
                     genre: 'Fantasy',
                     rating: 5,
                     explicit: true,
-                },
-                {
+                })
+            )
+            .then(() =>
+                knex('shows').insert({
                     name: 'South Park',
                     channel: 'Comedy Central',
                     genre: 'Comedy',
                     rating: 4,
                     explicit: true,
-                },
-                {
+                })
+            )
+            .then(() =>
+                knex('shows').insert({
                     name: 'Mad Men',
                     channel: 'AMC',
                     genre: 'Drama',
                     rating: 3,
                     explicit: false,
-                },
-            ]);
-        });
+                })
+            )
+    );
 };
